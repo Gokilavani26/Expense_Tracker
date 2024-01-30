efb12bb227b66a68e5138883513fd010a8295425
Gokilavani <88823054+Gokilavani26@users.noreply.github.com>
2024-01-30 11:35:20 +0530

    Delete expense-tracker-backend directory

--- expense-tracker-backend/models/expenses.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const expenseScheme =  new mongoose.Schema({
-    amount:Number,
-    desc:String,
-    title:String,
-});
-  
-const Expense = mongoose.model('Expense', expenseScheme);
-module.exports = Expense;
