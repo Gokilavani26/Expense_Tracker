a257b64e295b53e887b3885ec0d781f20266fb5f
Gokilavani <88823054+Gokilavani26@users.noreply.github.com>
2024-01-30 11:37:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Expense = require('./models/expenses');
+const Expense = require('./expenses');
 
 const app = express();
 const port = 3000;
